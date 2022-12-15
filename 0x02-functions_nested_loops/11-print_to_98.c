e5d8d52941edc732c9535f6ba48ebbf9f578c4d3
Uchechukwu Okoronkwo <uokoronkwo@gmail.com>
2022-12-14 16:22:03 -0800

    Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Print to 98
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+	for (; m <= 98; n++)
+	{
+	if ( n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf"%d", ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n>= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+}
+
+
