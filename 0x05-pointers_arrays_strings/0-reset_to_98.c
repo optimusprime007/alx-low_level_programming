e13f890948cfe3a1f42aed78b1f42fcf8a268a44
Uchechukwu Okoronkwo <uokoronkwo@gmail.com>
2022-12-19 13:25:06 -0800

    Battery Edit 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Takes a pointer to an int and update the value
+ * reset_to_98 - Takes a pointer to an int and update the value
  * @n: integer input
  * Return: blank
  */
